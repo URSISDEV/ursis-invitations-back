071766d48c01bfb48289984396c5a1815ee915de
Santinou1 <santitech445@gmail.com>
2025-11-09 15:00:58 -0300

    init: hola mundo invitations backend

---
+++ src/app.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('invitations')
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hola mundo desde Invitations API 🚀';
+  }
+}
